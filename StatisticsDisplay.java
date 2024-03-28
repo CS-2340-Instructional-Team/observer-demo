65c3169b64986cf24a36c6a60a065213a868e88d
CS-2340-Instructional-Team <143115839+CS-2340-Instructional-Team@users.noreply.github.com>
2024-03-28 18:07:03 -0400

    added observer files via upload

---
+++ StatisticsDisplay.java
@@ -0,0 +1,33 @@
+public class StatisticsDisplay implements Observer {
+    private float maxTemp = Float.MIN_VALUE;
+    private float minTemp = Float.MAX_VALUE;
+    private float tempSum = 0;
+    private int numReadings;
+    private WeatherData weatherData;
+
+    public StatisticsDisplay(WeatherData weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    @Override
+    public void update(float temperature, float humidity, float pressure) {
+        tempSum += temperature;
+        numReadings++;
+
+        if (temperature > maxTemp) {
+            maxTemp = temperature;
+        }
+
+        if (temperature < minTemp) {
+            minTemp = temperature;
+        }
+
+        display();
+    }
+
+    public void display() {
+        System.out.println("Avg/Max/Min temperature = " + (tempSum / numReadings)
+            + "/" + maxTemp + "/" + minTemp);
+    }
+}
